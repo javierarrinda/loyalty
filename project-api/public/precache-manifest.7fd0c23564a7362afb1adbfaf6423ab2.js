1be7e8bacddd47ac5d1034ecf34d09f4f719a54c
Javier <jarrindaulivi4@gmail.com>
2018-12-21 09:25:12 -0500

    fifth

---
+++ project-api/public/precache-manifest.7fd0c23564a7362afb1adbfaf6423ab2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "325001cdf20fd37fd646",
+    "url": "/static/css/main.dce090a5.chunk.css"
+  },
+  {
+    "revision": "325001cdf20fd37fd646",
+    "url": "/static/js/main.325001cd.chunk.js"
+  },
+  {
+    "revision": "00b846dda453dbddda0d",
+    "url": "/static/js/1.00b846dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94150fa47cd57f91bd73fa2e7c2a2475",
+    "url": "/index.html"
+  }
+];
